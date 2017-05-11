64192bb4a59aa93b61142df3f017fa04659eed9b
Pascal Kerschke <kerschke@uni-muenster.de>
2017-05-11 15:29:57 +0000

    version 1.6

--- R/gui_BBOBImport.R
+++ R/gui_BBOBImport.R
@@ -20,8 +20,17 @@ BBOBImportPage = function(id) {
     shiny::sidebarPanel(
       shiny::fileInput(ns("BBOB_import_file"), label = "File to import"),
       shiny::numericInput(ns("BBOB_import_replication"), label = "Replications", value = 1),
-      shiny::selectInput(ns("BBOB_import_featureSet"),label = "Feature Set", choices = listAvailableFeatureSets()),
-      shiny::textInput(ns("BBOB_block_input"), label = "Blocks (comma sperated per dimension)", value = 2),
+      shiny::selectInput(ns("BBOB_import_featureSet"), label = "Feature Set",
+        choices = c("all Features", listAvailableFeatureSets()), selected = "cm_angle"),
+      shiny::splitLayout(
+        shiny::textInput(ns("BBOB_block_input"), label = "Blocks", value = 2),
+        shiny::selectInput(ns("BBOB_sampletype"), label = "Sample type", choices = c("random","lhs"))
+      ),
+      # put lower and upper bound in one line
+      shiny::splitLayout(
+        shiny::textInput(ns("BBOB_samplelow"), label = "Lower bound", value = "0"),
+        shiny::textInput(ns("BBOB_sampleup"), label = "Upper bound", value = "1")
+      ),
       shiny::sliderInput(ns("BBOB_ssize"), "Sample size", min = 10, max = 5000, value = 100),
       shiny::downloadButton(ns('BBOB_import_downloadData'), 'Download')
     ),
@@ -51,10 +60,23 @@ BBOBImport = function(input, output, session, stringsAsFactors) {
     features = data.frame()
     # load values from uploaded file
     importdata = utils::read.csv(input$BBOB_import_file$datapath, sep = ",", header = TRUE)
+    # transform the text input for lower and upper bound to an vector with the respective values
+    shiny::validate(
+      shiny::need(try({
+        lowerbound = eval(parse(text = paste("c(", input$BBOB_samplelow, ")")))
+      }), "Please insert valid lowerbound defintion") %then%
+        shiny::need(try({
+          upperbound = eval(parse(text = paste("c(", input$BBOB_sampleup, ")")))
+        }), "Please insert valid upperbound defintion")
+    )
+    # get ctrl values for creation of initial sample
+    ctrl = list(init_sample.type = input$BBOB_sampletype,
+      init_sample.lower = lowerbound,
+      init_sample.upper = upperbound)
     # calculate features for all rows of input file
     for (i in seq_row(importdata)) {
       for (r in seq_len(input$BBOB_import_replication)) {
-        X = createInitialSample(n.obs = input$BBOB_ssize, dim = importdata[i, 3L])
+        X = createInitialSample(n.obs = input$BBOB_ssize, dim = importdata[i, 3L], control = ctrl)
         f = smoof::makeBBOBFunction(dimension = importdata[i, 3L], fid = importdata[i, 1L], iid = importdata[i, 2L])
         y = apply(X, 1L, f)
         # check if input for blocks is available
@@ -70,10 +92,17 @@ BBOBImport = function(input, output, session, stringsAsFactors) {
           feat.object = createFeatureObject(X = X, y = y, fun = f)
         }
         # calculate the features
-        features_l = data.frame(fid = importdata[i, 1L], iid = importdata[i, 2L],
-          dim = importdata[i, 3L], rep = r,
-          calculateFeatureSet(feat.object, set = input$BBOB_import_featureSet,
-            control = list(ela_curv.sample_size = min(200L, feat.object$n.obs))))
+        if (input$BBOB_import_featureSet == "all Features") {
+          features_l = data.frame(fid = importdata[i, 1L], iid = importdata[i, 2L],
+            dim = importdata[i, 3L], rep = r,
+            calculateFeatures(feat.object,
+              control = list(ela_curv.sample_size = min(200L, feat.object$n.obs))))
+        } else {
+          features_l = data.frame(fid = importdata[i, 1L], iid = importdata[i, 2L],
+            dim = importdata[i, 3L], rep = r,
+            calculateFeatureSet(feat.object, set = input$BBOB_import_featureSet,
+              control = list(ela_curv.sample_size = min(200L, feat.object$n.obs))))
+        }
         features = rbind(features, features_l)
       }
     }
